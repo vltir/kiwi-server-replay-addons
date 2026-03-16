3c590149ee7d711fac4b985578dd76ccd47783a7
vltir <87820976+vltir@users.noreply.github.com>
2026-03-16 14:53:07 +0100

    Initial Commit

---
+++ src/main/java/com/vltir/replay/KiwiServerReplayAddons.java
@@ -0,0 +1,10 @@
+package com.vltir.replay;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KiwiServerReplayAddons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
